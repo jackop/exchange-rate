7dd8fc82addf5409f4ae21874b9f3fe40803fe9d
Jacek Koprowski <koperingnet@op.pl>
2019-10-19 11:11:18 +0200

    [master] change to get range date - exchange rate

--- exchangerateApp/src/main/java/com/jackop/exchangerate/Application.java
+++ exchangerateApp/src/main/java/com/jackop/exchangerate/Application.java
@@ -1,49 +1,46 @@
 package com.jackop.exchangerate;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jackop.exchangerate.models.Table;
-import com.jackop.exchangerate.services.CSVService;
-import com.jackop.exchangerate.utils.StringUtils;
+import com.jackop.exchangerate.services.FetchService;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
 public class Application {
 
-  private static final String TABLE_A = "http://api.nbp.pl/api/exchangerates/tables/A";
-  private static final String TABLE_C = "http://api.nbp.pl/api/exchangerates/tables/C";
-  private static final String[] tables = {TABLE_A, TABLE_C};
+  private static final String EXHANGE_RATE_URL = "http://api.nbp.pl/api/exchangerates/tables/";
+  private static final String[] tables = {"A", "C"};
   private static final String CURRENCY_CODE = "HKD";
   private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
   private static final ObjectMapper mapper = new ObjectMapper();
-  private static Random rand;
-
-  static {
-    try {
-      rand = SecureRandom.getInstanceStrong();
-    } catch (NoSuchAlgorithmException e) {
-      LOGGER.warning(e.getMessage());
-    }
-  }
+  private static final FetchService fetchService = new FetchService();
 
   public static void main(String[] args) throws IOException {
     String selectedTableUrl = pickUpTableRandomly();
-    fetch(selectedTableUrl);
+    String url = EXHANGE_RATE_URL + selectedTableUrl + "/2019-10-10/2019-10-15";
+    System.out.println(url);
+    fetch(url);
   }
 
   private static void fetch(String url) throws IOException {
-    String response = StringUtils.prepareJsonResponse(url);
-    Table currencyTable = mapper.readValue(response, Table.class);
-    currencyTable.getRates().stream()
-      .filter(Objects::nonNull)
-      .filter(rate -> rate.getCode().equals(CURRENCY_CODE))
-      .forEach(rate -> CSVService.parseObjectForCsv(CURRENCY_CODE, currencyTable, rate));
+    String response = fetchService.fetch(url);
+    System.out.println(response);
+    JavaType type = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Table.class);
+    List<Table> currencyTable = mapper.readValue(response, type);
+    currencyTable.stream().forEach(table -> System.out.println(table.getEffectiveDate()));
+//    System.out.println(currencyTable);
+//    currencyTable.getRates().stream()
+//      .filter(Objects::nonNull)
+//      .filter(rate -> rate.getCode().equals(CURRENCY_CODE))
+//      .forEach(rate -> CSVService.parseObjectForCsv(CURRENCY_CODE, currencyTable, rate));
   }
 
   private static String pickUpTableRandomly() {
+    Random rand = new Random();
     int randomNumber = rand.nextInt(Application.tables.length);
 
     return Application.tables[randomNumber];
